e8c26033ac23b2c4988a34ead75e745e90290a4d
wildone <max.barrass@gmail.com>
2019-09-23 22:05:20 +1000

    add ability to download files to install updated params fixed error reports

--- library/pyaem_command.py
+++ library/pyaem_command.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python
 
-from ansible.module_utils.basic import *
-
 import docker
+import urllib
+
+from ansible.module_utils.basic import *
 
 
 def main():
     fields = {
-        "host": {"default": True, "type": "str"},
-        "port": {"default": True, "type": "str"},
-        "group_name": {"default": True, "type": "str"},
-        "package_name": {"default": True, "type": "str"},
-        "package_version": {"default": True, "type": "str"},
-        "aem_username": {"default": True, "type": "str"},
-        "aem_password": {"default": True, "type": "str", "no_log": True},
-        "file_path": {"default": False, "type": "str"},
-        "api_command": {"default": True, "type": "str"}
+        "host": {"required": True, "type": "str"},
+        "port": {"required": True, "type": "str"},
+        "group_name": {"required": True, "type": "str"},
+        "package_name": {"required": True, "type": "str"},
+        "package_version": {"required": True, "type": "str"},
+        "aem_username": {"required": True, "type": "str"},
+        "aem_password": {"required": True, "type": "str", "no_log": True},
+        "file_path": {"required": False, "type": "str"},
+        "file_url": {"required": False, "type": "str"},
+        "file_name": {"required": False, "type": "str"},
+        "file_url_password": {"required": False, "type": "str"},
+        "file_url_username": {"required": False, "type": "str"},
+        "api_command": {"required": True, "type": "str"},
+        "docker_url": {"required": False, "type": "str", "default": "unix://var/run/docker.sock"}
     }
 
     module = AnsibleModule(argument_spec=fields)
@@ -28,32 +34,54 @@ def main():
         package_name=module.params['package_name'],
         package_version=module.params['package_version'],
         file_path=module.params['file_path'],
+        file_url=module.params['file_url'],
+        file_name=module.params['file_name'],
+        file_url_password=module.params['file_url_password'],
+        file_url_username=module.params['file_url_username'],
 
         aem_username=module.params['aem_username'],
         aem_password=module.params['aem_password'],
         api_command=module.params['api_command'],
+        docker_url=module.params['docker_url'],
 
-        container_image='aemdesign/ansible-playbook',
+        container_image='aemdesign/ansible-playbook:centos7',
+        container_image_volume='/ansible/playbooks',
         container_entrypoint='/usr/bin/python',
         container_user='root',
         container_remove=True,
         container_privileged=True,
-        container_volume="%s:/ansible/playbooks:ro" % (
+        container_volume="%s:/ansible/playbooks" % (
             '/tmp' if module.params['file_path'] is None else module.params['file_path'])
     )
 
-    client = docker.Client()
+    client = docker.APIClient(base_url=params["docker_url"])
 
     hostconfig = client.create_host_config()
 
+    params['getfile'] = ""
+    getfile = ""
+
+    if params['file_url']:
+        if params['file_url_username']:
+            getfile = "password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm(); " \
+                      "password_mgr.add_password(None, " \
+                      "'%(file_url)s', " \
+                      "'%(file_url_username)s', " \
+                      "'%(file_url_password)s'); " \
+                      "auth_handler = urllib.request.HTTPBasicAuthHandler(password_mgr); " \
+                      "opener = urllib.request.build_opener(auth_handler); " \
+                      "urllib.request.install_opener(opener); "
+        getfile += "urllib.request.urlretrieve('%(file_url)s', '%(file_name)s');"
+        params['getfile'] = getfile % params
+
     # create pyaem2 init command
-    params[
-        'pyaem_init'] = "aem = pyaem2.PyAem2('%(aem_username)s', '%(aem_password)s', '%(host)s', '%(port)s'); " % params
-    # params['pyaem_error'] = "if result.is_failure(): print(json.dumps({ 'failed': True, 'msg': result.message })); else: print(json.dumps({ 'msg': result.message }));"
-    params[
-        'pyaem_error'] = "print( json.dumps({ 'failed': True, 'msg': result.message }) if result.is_failure() else json.dumps({ 'msg': result.message }) )"
+    params['pyaem_init'] = "aem = pyaem2.PyAem2('%(aem_username)s'," \
+                           " '%(aem_password)s', '%(host)s', '%(port)s'); " % params
+    params['pyaem_error'] = "print( json.dumps({ 'failed': True, 'msg': result.message }) if " \
+                            "result.is_failure() else json.dumps({ 'msg': result.message }) ) "
 
     params['pyaem_cmd'] = ''
+    params['pyaem_cmd1'] = ''
 
     # create pyaem function command
     if any(True for match in
@@ -64,8 +92,8 @@ def main():
            ]
            if match in params['api_command']
            ):
-        params[
-            'pyaem_cmd'] = "result = aem.%(api_command)s('%(group_name)s', '%(package_name)s', '%(package_version)s' ); " % params
+        params['pyaem_cmd'] = "result = aem.%(api_command)s('%(group_name)s', '%(package_name)s', " \
+                              "'%(package_version)s' ); " % params
     elif 'upload_package_sync' in params['api_command']:
         if not params['file_path']:
             module.fail_json(msg={
@@ -77,26 +105,41 @@ def main():
         hostconfig = client.create_host_config(privileged=params['container_privileged'], binds=[
             params['container_volume']
         ])
-        params[
-            'pyaem_cmd'] = "result = aem.upload_package_sync('%(group_name)s', '%(package_name)s', '%(package_version)s', '/ansible/playbooks', force = 'true'); " % params
+        params['pyaem_cmd'] = "result = aem.upload_package_sync(" \
+                              "'%(group_name)s', " \
+                              "'%(package_name)s', " \
+                              "'%(package_version)s', " \
+                              "'/ansible/playbooks', " \
+                              "force = 'true'); " % params
     else:
-        module.fail_json(msg={
-            'msg': 'Unsuported api_command',
-            'api_command': params['api_command']
-        })
+        module.fail_json(
+            failed=True,
+            msg={
+                'msg': 'Unsuported api_command',
+                'api_command': params['api_command']
+            }
+        )
         return False
 
     # join pyaem init and function commands
-    params['pyaemscript'] = "-c \"import json; import pyaem2; %(pyaem_init)s %(pyaem_cmd)s %(pyaem_error)s \"" % params
+    params['pyaemscript'] = "-c \"import json; import pyaem2; import urllib.request; " \
+                            "%(getfile)s " \
+                            "%(pyaem_init)s " \
+                            "%(pyaem_cmd)s " \
+                            "%(pyaem_error)s \"" % params
 
     # create helper for debugging
-    params[
-        'pyaemscriptmanual'] = "docker run -it --privileged --entrypoint %(container_entrypoint)s -v %(container_volume)s %(container_image)s %(pyaemscript)s" % params
+    params['pyaemscriptmanual'] = "docker run -it --privileged " \
+                                  "--entrypoint %(container_entrypoint)s " \
+                                  "-v %(container_volume)s " \
+                                  "%(container_image)s " \
+                                  "%(pyaemscript)s" % params
 
     container = client.create_container(
         image=params['container_image'],
         command=params['pyaemscript'],
         user=params['container_user'],
+        volumes=[params['container_image_volume']],
         entrypoint=[params['container_entrypoint']],
         host_config=hostconfig
     )
@@ -104,7 +147,13 @@ def main():
 
     logs = ''
     for char in client.logs(container=container, stream=True):
-        logs = logs + char
+        logs = logs + char.decode('utf-8')
+
+    logsresult = dict(
+        failed='failed' in logs,
+        msg=logs,
+        logerror=''
+    )
 
     if params['container_remove']:
         client.remove_container(container=container.get('Id'))
@@ -117,10 +166,22 @@ def main():
         'status': containerstart
     }
 
-    module.exit_json(msg={
-        'failed': 'error' in logs,
-        'msg': result
-    })
+    if logsresult['failed']:
+        module.fail_json(
+            failed=True,
+            changed=False,
+            msg=logsresult['msg'],
+            logerror=logsresult['logerror'],
+            result=result
+        )
+    else:
+        module.exit_json(
+            failed=logsresult['failed'],
+            changed=True,
+            msg=logsresult['msg'],
+            logerror=logsresult['logerror'],
+            result=result
+        )
     return True
 
 
